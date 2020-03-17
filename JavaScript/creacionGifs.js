8fbbc20b596a585a7b45de065c25022902bb4fde
Manuel Santiago Gómez Mancipe <55415338+xManuelGMx@users.noreply.github.com>
2020-03-16 21:08:35 -0500

    Add files via upload

---
+++ JavaScript/creacionGifs.js
@@ -0,0 +1,181 @@
+//Capturar video desde el navegador
+let video = document.querySelector("video");
+function capturar() {
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true
+    })
+    .then(stream => {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(console.error);
+    let info = document.querySelector("div.container");
+    info.removeAttribute("style");
+    let capturador = document.querySelector("div.captura");
+    capturador.removeAttribute("style");
+
+}
+// Cancelar grabación :v
+function cancelar() {
+    document.querySelector(".container").remove();
+}
+// Comenzar grabación
+function grabar() {
+    recorder = RecordRTC(video.srcObject, {
+        type: 'gif',
+        frameRate: 1,
+        quality: 10,
+        width: 360,
+        hidden: 240,
+        onGifRecordingStarted: () => {
+            console.log('started');
+        }
+    });
+    recorder.startRecording();
+    let imagen = document.querySelector(".capturar button img");
+    imagen.src = "img/recording_dark.svg";
+    imagen.style.filter = "contrast(0) brightness(0);";
+    let botones = document.querySelectorAll(".capturar button");
+    botones[0].style.background = "#FF6161";
+    botones[1].style.background = "#FF6161";
+    botones[1].innerHTML = "Listo";
+    botones[0].setAttribute("onclick", "detener()");
+    botones[1].setAttribute("onclick", "detener()");
+
+    document.querySelector("div.captura div.barraTitulo").innerHTML = 'Capturando Tu Guifo<img src="img/button3.svg" alt="Botón eliminar" width="16" height="16">';
+}
+let blob;
+let gifActual;
+let form;
+let url;
+function detener() {
+    recorder.stopRecording(function() {
+        blob = recorder.getBlob();
+        form = new FormData();
+        form.append('file', blob, 'myGif.gif');
+        gifActual = form.get('file');
+        url = URL.createObjectURL(blob);
+        
+        // Colocar gif
+        let img = document.createElement('img');
+        img.src = url;
+        img.style.position = "absolute";
+        img.style.height = "100%";
+
+        let divVideo = document.querySelector("div.video");
+        let video = document.querySelector("video")
+        divVideo.insertBefore(img, video);
+
+        document.querySelector("button.repeat").setAttribute("style", "display: block");
+        document.querySelector("button.upload").setAttribute("style", "display: block");
+        document.querySelector("div.captura div.capturar button:nth-child(1)").setAttribute("style", "display: none");
+        document.querySelector("div.captura div.capturar button:nth-child(2)").setAttribute("style", "display: none");
+
+        document.querySelector("div.captura div.barraTitulo").innerHTML = "Vista Previa";
+    });
+}
+let arrKeys = [];
+// crear campo del gif
+window.onload = function() {
+    for (let i = 1; i < localStorage.length; i++) {
+        let divLista = document.querySelector("div.lista");
+        let divGif = document.createElement("div");
+        divGif.setAttribute("class", "gif");
+        let divImg = document.createElement("div");
+        divImg.setAttribute("class", "img");
+        
+        let img = document.createElement('img');
+        img.style.position = "absolute";
+        img.style.width = "100%";
+        img.style.height = "100%";
+        img.style.zIndex = 0;
+        let key = localStorage.key(i)
+        if (key.slice(0,3) === "Gif") {
+            img.src = localStorage.getItem(key);
+            divLista.insertBefore(divGif, document.querySelectorAll("div.gif")[0])
+            divGif.appendChild(divImg);
+            divImg.appendChild(img)
+        }
+    }
+}
+function repetir() {
+    // Reset
+    let imagen = document.querySelector(".capturar button img");
+    imagen.src = "img/camera.svg";
+    imagen.style.filter = "contrast(0) brightness(0);";
+    let botones = document.querySelectorAll(".capturar button");
+    botones[0].style.background = "#FF6161";
+    botones[1].style.background = "#FF6161";
+    botones[1].innerHTML = "Capturar";
+    botones[0].setAttribute("onclick", "grabar()");
+    botones[1].setAttribute("onclick", "grabar()");
+    document.querySelector("button.repeat").setAttribute("style", "display: none");
+    document.querySelector("button.upload").setAttribute("style", "display: none");
+    document.querySelector("div.captura div.capturar button:nth-child(1)").setAttribute("style", "display: block");
+    document.querySelector("div.captura div.capturar button:nth-child(2)").setAttribute("style", "display: block");
+    document.querySelector("div.video img").remove();
+    capturar();
+}
+function guardar() {
+    let i = localStorage.getItem("0");
+    localStorage.setItem("Gif"+i,url);
+    localStorage.setItem("0",++i);
+    video.remove();
+    // crear campo del gif
+    let divLista = document.querySelector("div.lista");
+    let divGif = document.createElement("div");
+    divGif.setAttribute("class", "gif");
+    let divImg = document.createElement("div");
+    divImg.setAttribute("class", "img");
+    // divLista.appendChild(divGif);
+    // divGif.appendChild(divImg);
+
+    document.querySelector("div.captura div.barraTitulo").innerHTML = 'Subiendo Guifo<img src="img/button3.svg" alt="Botón eliminar" width="16" height="16">';
+    let divVideo = document.querySelector("div.video");
+    let img = document.querySelector("div.video img");
+    img.src = "img/globe_img.png";
+    img.style.width = "auto";
+    img.style.height = "3em";
+    img.style.opacity = "1";
+    img.style.transform = "translateY(-45px)";
+    divVideo.setAttribute("style", "background-color: white");
+    document.querySelector("button.upload").remove();
+    document.querySelector("button.repeat").innerHTML = "Cancelar";
+
+    let span = document.createElement("span");
+    span.classList.add("text");
+    span.innerHTML = "Estamos subiendo tu guifo…";
+    document.querySelector("div.video").appendChild(span);
+
+    setTimeout(() => {
+        document.querySelector("div.captura").remove();
+        document.querySelector("div.exito").removeAttribute("style");
+        document.querySelector("div.vistaPrevia div.img img").src = url;
+    },3000);
+}
+function copy() {
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement("input");
+
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute("value", url);
+
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+
+    // Selecciona el contenido del campo
+    aux.select();
+
+    // Copia el texto seleccionado
+    document.execCommand("copy");
+
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
+}
+function download() {
+    alert("Proximamente!!")
+}
+function recargar() {
+    window.location.reload();
+}
---
+++ JavaScript/javascript.js
@@ -0,0 +1,177 @@
+// Menú desplegable de los temas
+function mostrar() {
+    let menuTemasActual = document.getElementById("temas").style.display;
+    let menuTemasMostrar = "flex";
+    let menuTemasEsconder = "none";
+    if(menuTemasActual !== menuTemasMostrar){
+        document.getElementById("temas").style.setProperty('display',menuTemasMostrar);
+    }else{
+        document.getElementById("temas").style.setProperty('display',menuTemasEsconder);
+    }
+}
+// Tema oscuro de la página (swicth)
+var light = true;
+function night(){
+    light = false;
+    let root = document.documentElement;
+    // ---colorFondo------------------------------------------------------------------
+    root.style.setProperty('--colorFondo',"#110938");
+    // ---colorBarraSecundario------------------------------------------------------------------
+    root.style.setProperty('--colorBarraSecundario',"#EE3EFE");
+    // ---colorBarraPrimario------------------------------------------------------------------
+    root.style.setProperty('--colorBarraPrimario',"#2E32FB");
+    // ---colorBoton1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton1',"#EE3EFE");
+    // ---colorBoton1Borde1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton1Borde1',"#A72CB3");
+    // ---colorVentanaBorde1------------------------------------------------------------------
+    root.style.setProperty('--colorVentanaBorde1',"#8F8F8F");
+    // ---colorBoton2Borde1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton2Borde1',"#2124B3");
+    // ---colorBoton2Fondo------------------------------------------------------------------
+    root.style.setProperty('--colorBoton2Fondo',"#2E32FB");
+    // ---colorDivisionBorde------------------------------------------------------------------
+    root.style.setProperty('--colorDivisionBorde',"#2E32FB");
+    // ---colorDivisionBorde2------------------------------------------------------------------
+    root.style.setProperty('--colorDivisionBorde2',"#110038");
+    // ---colorGifBorde1------------------------------------------------------------------
+    root.style.setProperty('--colorGifBorde1',"#5C5C5C");
+    // ---colorTexto------------------------------------------------------------------
+    root.style.setProperty('--colorTexto',"#FFFFFF");
+    // ---colorTextoBoton------------------------------------------------------------------
+    root.style.setProperty('--colorTextoBoton',"#8F8F8F");
+    // ---fondoVentana------------------------------------------------------------------
+    root.style.setProperty('--fondoVentana',"#B4B4B4");
+    // ---Menú temas------------------------------------------------------------------
+    let botonDay = document.querySelector("button.day");
+    botonDay.style.background = "#F0F0F0";
+    botonDay.style.border = "1px solid #808080";
+    botonDay.style.boxShadow = "inset -1px -1px 0 0 #B4B4B4, inset 1px 1px 0 0 #FFFFFF";
+    let botonNight = document.querySelector("button.night");
+    botonNight.style.background = "#2E32FB";
+    botonNight.style.border = "1px solid rgba(51,53,143,0.20)";
+    botonNight.style.boxShadow = "inset -1px -1px 0 0 #E6DCE4, inset 1px 1px 0 0 #FFFFFF";
+    // ---Imágenes------------------------------------------------------------------
+    document.getElementById("logo").src = "img/gifOF_logo_dark.png";
+    document.getElementById("lista").style.setProperty('filter',"invert(1)");
+    let input = document.querySelector("input").value;
+    if(input.length > 0){
+        document.getElementById("lupa").setAttribute("src", "img/lupa_light.svg");
+    }else{
+        document.getElementById("lupa").setAttribute("src", "img/Combined Shape.svg");
+    }
+}
+function day() {
+    light = true;
+    let root = document.documentElement;
+    // ---colorFondo------------------------------------------------------------------
+    root.style.setProperty('--colorFondo',"#FEF4FD");
+    // ---colorBarraSecundario------------------------------------------------------------------
+    root.style.setProperty('--colorBarraSecundario',"#F7C9F3");
+    // ---colorBarraPrimario------------------------------------------------------------------
+    root.style.setProperty('--colorBarraPrimario',"#4180F6");
+    // ---colorBoton1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton1',"#F7C9F3");
+    // ---colorBoton1Borde1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton1Borde1',"#997D97");
+    // ---colorVentanaBorde1------------------------------------------------------------------
+    root.style.setProperty('--colorVentanaBorde1',"#B4B4B4");
+    // ---colorBoton2Borde1------------------------------------------------------------------
+    root.style.setProperty('--colorBoton2Borde1',"#284F99");
+    // ---colorBoton2Fondo------------------------------------------------------------------
+    root.style.setProperty('--colorBoton2Fondo',"#4180F6");
+    // ---colorDivisionBorde------------------------------------------------------------------
+    root.style.setProperty('--colorDivisionBorde',"#E6BBE2");
+    // ---colorDivisionBorde2------------------------------------------------------------------
+    root.style.setProperty('--colorDivisionBorde2',"#80687D");
+    // ---colorGifBorde1------------------------------------------------------------------
+    root.style.setProperty('--colorGifBorde1',"#B4B4B4");
+    // ---colorTexto------------------------------------------------------------------
+    root.style.setProperty('--colorTexto',"#110038");
+    // ---colorTextoBoton------------------------------------------------------------------
+    root.style.setProperty('--colorTextoBoton',"#B4B4B4");
+    // ---fondoVentana------------------------------------------------------------------
+    root.style.setProperty('--fondoVentana',"#E6E6E6");
+    // ---Menú temas------------------------------------------------------------------
+    let botonNight = document.querySelector("button.night");
+    botonNight.style.background = "#F0F0F0";
+    botonNight.style.border = "1px solid #808080";
+    botonNight.style.boxShadow = "inset -1px -1px 0 0 #B4B4B4, inset 1px 1px 0 0 #FFFFFF";
+    let botonDay = document.querySelector("button.day");
+    botonDay.removeAttribute("style");
+    // ---Imágenes------------------------------------------------------------------
+    document.getElementById("logo").src = "img/gifOF_logo.png";
+    document.getElementById("lista").style.setProperty('filter',"invert(0)");
+    let input = document.querySelector("input").value;
+    if(input.length > 0){
+        document.getElementById("lupa").setAttribute("src", "img/lupa.svg");
+    }else{
+        document.getElementById("lupa").setAttribute("src", "img/lupa_inactive.svg");
+    }
+}
+// Mostar sugerencias de búsqueda
+function comprobar(){
+    let input = document.querySelector("input").value;
+    if(input.length > 0){
+        document.getElementById("sugerenciasBuscador").style.setProperty('visibility',"visible");
+        document.getElementById("buscador").style.setProperty('background-color',"var(--colorBoton1)");
+        document.getElementById("texto").style.setProperty('color',"var(--colorTexto)");
+        if(light){
+            document.getElementById("lupa").setAttribute("src", "img/lupa.svg");
+        }else{
+            document.getElementById("lupa").setAttribute("src", "img/lupa_light.svg");
+        }
+    }else{
+        document.getElementById("sugerenciasBuscador").style.setProperty('visibility',"hidden");
+        document.getElementById("buscador").style.setProperty('background-color',"var(--fondoVentana)");
+        document.getElementById("texto").style.removeProperty('color');
+        if(light){
+            document.getElementById("lupa").setAttribute("src", "img/lupa_inactive.svg");
+        }else{
+            document.getElementById("lupa").setAttribute("src", "img/Combined Shape.svg");
+        }
+    }
+}
+//Ir a crear gif
+function crearGif() {
+    if(window.location.pathname.slice(-17) !== "/creacionGif.html"){
+        window.location = "creacionGif.html";
+    }
+}
+// Devuelta al index.html
+function index() {
+    window.location = "inicio.html";
+    console.log("inicio");
+}
+function inicio() {
+    document.querySelector("body").scrollIntoView({
+        behavior: "smooth"
+    })
+}
+// Historial de búsqueda
+let historial = [];
+function guardar() {
+    let box = document.querySelector("div.historial");
+    let boton = document.createElement("button");
+    boton.classList.add("boton2");
+    if (historial.length < 20) {
+        historial.unshift(input)
+    }else{
+        historial.pop();
+        historial.unshift(input);
+    }
+    boton.innerText = "#"+input;
+    boton.setAttribute("onclick", "volverBuscar('"+input+"')");
+    box.appendChild(boton);
+}
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        if (window.location.pathname.slice(-11) === "/inicio.html") {
+            document.querySelector("button.top").style.transform = "translateY(0px)"
+        }
+    }else{
+        if (window.location.pathname.slice(-11) === "/inicio.html") {
+            document.querySelector("button.top").style.transform = "translateY(100px)"
+        }
+    }
+})
